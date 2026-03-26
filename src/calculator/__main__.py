45a9a4cab61b58291026b88822139fb7cc5d81af
dood345 <dood345@gmail.com>
2026-03-26 15:57:31 -0500

    build: moved main from redundant main file to module __main__.py

--- src/calculator/__main__.py
+++ src/calculator/__main__.py
@@ -1,4 +1,23 @@
-from calculator.main import main
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from calculator.controller import CalculatorController
+from calculator.model import CalculatorModel
+from calculator.view import CalculatorView
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    view = CalculatorView()
+    model = CalculatorModel()
+
+    CalculatorController(view, model)
+
+    view.show()
+    sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
